108629867005bf5f9c383f491fcbd977fc4482bb
yihan <yihan.zhou19@gmail.com>
2025-08-12 11:37:54 +0800

    [feat] hecv_vaapi encoder

--- orbbec_camera/src/image_publisher.cpp
+++ orbbec_camera/src/image_publisher.cpp
@@ -35,6 +35,29 @@ size_t image_rcl_publisher::get_subscription_count() const {
 image_transport_publisher::image_transport_publisher(rclcpp::Node& node,
                                                      const std::string& topic_name,
                                                      const rmw_qos_profile_t& qos) {
+  std::string param_base = topic_name;
+  std::replace(param_base.begin(), param_base.end(), '/', '.');
+  param_base = "." + param_base + ".ffmpeg.";
+
+  // Declare all FFmpeg publisher parameters
+  try {
+    node.declare_parameter(param_base + "encoding", "hevc_vaapi");
+    node.declare_parameter(param_base + "bit_rate", 8242880);
+    node.declare_parameter(param_base + "qmax", 10);
+    node.declare_parameter(param_base + "gop_size", 10);
+
+    auto all_params = node.list_parameters({}, 0).names;
+    RCLCPP_INFO(node.get_logger(), "=== ALL NODE PARAMETERS ===");
+    for (const auto& p : all_params) {
+      if (p.find("ffmpeg") != std::string::npos || p.find("encoder") != std::string::npos) {
+        RCLCPP_INFO(node.get_logger(), "  %s", p.c_str());
+      }
+    }
+    RCLCPP_INFO(node.get_logger(), "Pre-declared FFmpeg parameters with base: %s", param_base.c_str());
+  } catch (const rclcpp::exceptions::ParameterAlreadyDeclaredException &) {
+    RCLCPP_INFO(node.get_logger(), "FFmpeg parameters already declared for: %s", param_base.c_str());
+  }
+
   image_publisher_impl = std::make_shared<image_transport::Publisher>(
       image_transport::create_publisher(&node, topic_name, qos));
 }
